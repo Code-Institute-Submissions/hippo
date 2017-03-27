67375cf9f76272cca18668dd70cff2e3c080b1d0
FutoiSaru <futoisaru@gmail.com>
2017-03-27 11:52:52 +0100

    Created routing and added homepage

---
+++ js/app.js
@@ -0,0 +1,10 @@
+angular.module('HippoSite', ['ngRoute', 'RouteControllers']);
+
+angular.module('HippoSite').config(function($locationProvider, $routeProvider) {
+	$locationProvider.html5Mode(true);
+
+	$routeProvider.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'HomeController'
+	});
+});
